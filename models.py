fd76cfb01bb749e06a828d4543b5d8bc056b5642
Merzlyakov Danila <merzlyakovdanila@gmail.com>
2022-11-05 01:22:04 +0300

    Model created

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class Requests(Base):
 
 
 class Employees(Base):
-    __tablename__ = 'requests'
+    __tablename__ = 'employees'
     __table_args__ = {
         'schema': 'public',
     }
@@ -32,3 +32,72 @@ class Employees(Base):
     lastName = Column(Integer)
     firstName = Column(Integer)
     secondName = Column(String)
+
+
+class Projects(Base):
+    __tablename__ = 'projects'
+    __table_args__ = {
+        'schema': 'public',
+    }
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    owner = Column(String)
+    manager = Column(String)
+    startDate = Column(DATETIME)
+    endDate = Column(DATETIME)
+    title = Column(String)
+    price = Column(Integer)
+    status = Column(String)
+
+
+class Role(Base):
+    __tablename__ = 'role'
+    __table_args__ = {
+        'schema': 'public',
+    }
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String)
+    grade = Column(Integer)
+
+
+class Skills(Base):
+    __tablename__ = 'skills'
+    __table_args__ = {
+        'schema': 'public',
+    }
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String)
+    level = Column(Integer)
+
+
+class Rookies(Base):
+    __tablename__ = 'rookies'
+    __table_args__ = {
+        'schema': 'public',
+    }
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    role_id = Column(Integer, ForeignKey(''))
+    skill_id = Column(Integer, ForeignKey(''))
+    firstName = Column(String)
+    lastName = Column(String)
+    secondName = Column(String)
+    vacancy_id = Column(Integer, ForeignKey(''))
+
+
+class EmployeesSkills(Base):
+    __tablename__ = 'employeesSkills'
+    __table_args__ = {
+        'schema': 'public',
+    }
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    skill_id = Column(Integer, ForeignKey(''))
+    employee_id = Column(Integer, ForeignKey(''))
+
+
+class EmployeesRole(Base):
+    __tablename__ = 'employeesRole'
+    __table_args__ = {
+        'schema': 'public',
+    }
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    role_id = Column(Integer, ForeignKey(''))
+    employee_id = Column(Integer, ForeignKey(''))
