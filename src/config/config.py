d915aadbab5b8db9c36a2290ace50029576c6db5
Merzlyakov Danila <merzlyakovdanila@gmail.com>
2022-12-24 00:28:34 +0300

    Create Docker Compose

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,5 @@
-DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/pro_db"
+from pydantic import Field
+# DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/pro_db"
+DATABASE_URL = Field(..., env='DATABASE_URL')
 TITLE_PROJECT = "Project IS"
-API_PREFIX = '/api'
+API_PREFIX = "/api"
